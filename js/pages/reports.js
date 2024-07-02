e8fc28150f5324276e6264dfe5eb83db9f3fc1d0
Marc Hudson <marc@mustimpress.com>
2024-07-02 07:32:15 +0100

    add data compile logic

--- js/pages/reports.js
+++ js/pages/reports.js
@@ -1,5 +1,17 @@
 var REPORTS = (function($, window, document, undefined) {
-    var pub = {};
+    var pub = {
+    	members: [],
+    	current: 0,
+    	total = 0,
+
+    	// reports data.
+    	genders: [],
+    	countrys: [],
+    	motives: [],
+    	ages: [],
+    	investors: [],
+    	joined: []
+    };
 
 
     // Load member data.
@@ -22,14 +34,88 @@ var REPORTS = (function($, window, document, undefined) {
 	// Callback function to process all members data.
 	pub.processData = (response) => {
 		console.log(response);
+
+		// set/update vars.
+		pub.members.push(response.data);
+		pub.current = pub.members.length;
+		pub.total = response.data[0].totalCount;
+
+		// update summary.
+		updateSummary(pub.current, pub.total);
+
+		// Load next round of data.
 		if (response.data[0].hasNextPage === "true") {
-			// Load next round of data.
 			alert('hasNextPage');
         	pub.getMemberData(response.data[0].endCursor);
 		}
 		else {
 			MAIN.thinking(false);
+
+			// Compile data.
+			$.each(pub.members, (i, member) => {
+				// gender.
+				if (member.customFields.gender) {
+					pub.genders[member.customFields.gender]++;
+				}
+				else {
+					pub.genders['Unknown']++;
+				}
+
+				// country.
+				if (member.customFields.country) {
+					pub.countrys[member.customFields.country]++;
+				}
+				else {
+					pub.countrys['Unknown']++;
+				}
+
+				// motives.
+				if (member.customFields.motive) {
+					$.each(member.customFields.motive.split('|'), (i, motive) => {
+						pub.motives[motive]++;
+					});
+				}
+				else {
+					pub.motives['Unknown']++;
+				}
+
+				// age.
+				if (member.customFields['date-of-birth']) {
+					let year = member.customFields['date-of-birth'].split('/')[2];
+					if (year) {
+						pub.ages[year]++;
+					}
+				}
+				else {
+					pub.ages['Unknown']++;
+				}
+
+				// investors.
+				if (member.customFields.investor) {
+					pub.investors[year]++;
+				}
+				else {
+					pub.investors['Unknown']++;
+				}
+
+				// joined.
+				if (member.customFields.createdAt) {
+					let date = new Date(member.customFields.createdAt);
+					let year = date.getFullYear();
+					let month = date.getMonth()+1;// returns 0-11, so add 1 to get 1-12.
+					pub.joined[`${year}-${month}`]++;
+				}
+
+				// plans.
+			});
 		}
+	}
+
+
+	pub.updateSummary = (current, total) => {
+		let $summary = $('#summary');
+		$('.current', $summary).text(current);
+		$('.total', $summary).text(total);
 	}
 
 
